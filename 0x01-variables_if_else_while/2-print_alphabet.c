27265dd8b08f58252a5be633d3490455a957b71d
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-17 14:53:02 -0800

    print alphabet in lower case followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower case followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('/n');
+	return (0);
+}
