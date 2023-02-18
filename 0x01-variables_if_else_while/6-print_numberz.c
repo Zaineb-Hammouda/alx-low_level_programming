6cc7c8f798264096fbb58d266e701399d81ee029
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-17 17:29:41 -0800

    print all single digit from 0 to 9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - entry point
+ * print all single digit number ofs base 10 from 0
+ * using putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int n;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
