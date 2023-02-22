3eb4a89453b6e8c18ce50992ed3a4c061143ff22
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-21 19:00:58 -0800

    2nd try

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
  * print_alphabet_x10 prints 10 times the alphabet in lc
  * _islower - checks for lowercase char
  * _isalpha - checks for alphabetic char
+ * print_sign - prints sign of num
  */
 
 #ifndef main_h
@@ -13,4 +14,5 @@ extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
 extern int _isalpha(int c);
+extern int print_sign(int n);
 #endif
