c758c6e6df639191d0830fe1cff7319a43ef3f60
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-23 12:27:12 -0800

    day of year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 extern void positive_or_negative(int i);
-extern int largest_number(int a, int b, int c)
+extern int largest_number(int a, int b, int c);
+extern void print_remaining_days(int month, int day, int year);
 
 #endif
