d77785b0170afac74dd0f6a05b08816d3beec32c
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-23 14:01:00 -0800

    check for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "./main.h"
+
+/**
+ * _isdigit - Checks for a digit
+ * Return: 1 success
+ * @c: c is the char to check (parameter)
+ */
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
