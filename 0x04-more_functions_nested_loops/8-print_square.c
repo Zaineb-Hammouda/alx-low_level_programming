ecbc88f790d25c95bfb11a0a9113fc5fc3a130b9
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-23 18:01:00 -0800

    draw square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "./main.h"
+
+/**
+ * print_square - draws a square
+ * Return: void
+ * @size: n is the size of the square
+ */
+
+void print_square(int size)
+{
+	int n, m;
+
+	if (size > 0)
+	{
+		for (n = 0; n < size; n++)
+		{
+			_putchar('#');
+			for (m = 0; m < size; m++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
