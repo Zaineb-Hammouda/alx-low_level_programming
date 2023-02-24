9acb6e435707cecceb18075f84a937a712c38d5c
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-23 16:21:21 -0800

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,16 +4,16 @@
  * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
  * Return: void
  */
+
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 48; i <= 57; i++)
 	{
-		_putchar(i);
-		if (i = 2 || i = 4)
+		if (i != 50 && i != 52)
 		{
-			continue;
+			_putchar(i);
 		}
 	}
 	_putchar('\n');
