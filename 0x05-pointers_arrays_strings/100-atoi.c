eb8999c54f160e451fb62ece96bd25757887b3fe
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-28 18:51:19 -0800

    convert string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - converts string to int
+ * Return: 0
+ * @s: the string to conver
+ */
+
+int _atoi(char *s)
+{
+	int i, length, j;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+	length = i - 1;
+
+	for (j = 0 ; j < length; j++)
+	{
+		if (s[i] == '-' || s[i] == '+')
+		{
+			if (s[i + 1] >= '0' && s[i + 1] <= '9')
+			{
+				putchar(s[i]);
+			}
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			putchar(s[i]);
+		}
+	}
+}
