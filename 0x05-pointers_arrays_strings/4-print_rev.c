ec77fe7829d76281f8fc38492d4778a69f15c093
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-02-28 17:05:31 -0800

    print a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,31 +6,17 @@
  * @s: the string to print
  */
 
-int _strlen(char *s);
-
 void print_rev(char *s)
 {
-	int i, length;
+	int i, j, length;
 
-	length = _strlen(s)
-
-	for (i = length - 1; i >= 0; i--)
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length = i;
+	}
+	for (j = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
-}
-/**
- * _strlen - returns the length of a string
- * Return: length of string
- * @s: string to measure
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; ++i)
-	;
-	return (i);
 }
