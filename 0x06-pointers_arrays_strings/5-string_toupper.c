d47df5be55a1dee9fde8617cbdcdc84777663df7
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-03-02 14:07:34 -0800

    change all lowercase letters of string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,16 +5,15 @@
  * Return: the adress to edited string
  * @str: the string to be changed
  */
-void *string_toupper(char *str)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
 	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 			str[i] = str[i] - 32;
-		}
+
 		i++;
 	}
 	return (str);
