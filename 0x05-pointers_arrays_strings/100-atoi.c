6a7432890c61522c9f7f393820f5683014f2f691
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-03-02 15:45:46 -0800

    convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,47 @@
 #include "main.h"
 
 /**
- * _atoi - converts string to int
- * Return: 0
- * @s: the string to conver
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-	int i, length, j;
+	int i, d, n, len, f, digit;
 
-	for (i = 0; s[i] != '\0'; ++i)
-		;
-	length = i - 1;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	for (j = 0 ; j < length; j++)
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (s[i] == '-' || s[i] == '+')
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i + 1] >= '0' && s[i + 1] <= '9')
-			{
-				return (s[i]);
-			}
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			return (s[i]);
-		}
+		i++;
 	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
+
