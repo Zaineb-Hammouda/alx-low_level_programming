babd6735d51de8edc89e025ddf2b08c4c415baba
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-03-07 21:58:34 -0800

    x power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x power y
+ * @x: the number
+ * @y: the power
+ * Return: x power y, -1 if y is < 0, 1 if y = 0
+ */
+int _pow_recursion(int x, int y)
+{
+	int i = 1, power;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	while (i <= y)
+	{
+		power = x * _pow_recursion(x, --y);
+		return (power);
+	}
+}
