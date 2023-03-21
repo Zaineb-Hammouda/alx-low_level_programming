4d58f1cb25c9189c1d8f3ba3b2515804183663e9
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-03-21 04:50:58 -0700

    2d array initialized to 0 with malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - allocates space for 2D array
+ * and initializes it to 0 with malloc
+ * Return: pointer to 2D array of ints
+ * or NULL if failed or if width or height is 0
+ * @width: number of columns
+ * @height: number of rows
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(height * sizeof(int *));
+	for (i = 0; i < height; i++)
+		arr[i] = malloc(width * sizeof(int));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
