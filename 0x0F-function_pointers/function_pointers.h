8427044bb276407cd737278591029a88573bc136
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-03-22 15:07:27 -0700

    fixed requirement

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#include <stddef.h>
-#ifndef function_pointers_h
-#define function_pointers_h
+#ifndef FUNC_POINTERS_H
+#define FUNC_POINTERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
@@ -11,6 +13,5 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 
 #endif
