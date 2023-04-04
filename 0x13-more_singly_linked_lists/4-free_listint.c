1eac259c65d649c9e2250021a6ecc12c097f352f
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-04-04 15:37:12 -0700

    function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: first node in list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *traverse = head;
+
+	while (traverse)
+	{
+		free(traverse);
+		traverse = traverse->next;
+	}
+}
