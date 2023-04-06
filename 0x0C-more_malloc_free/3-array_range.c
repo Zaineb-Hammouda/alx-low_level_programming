61d80274f754d436d31bd732578ee9926c674de0
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-04-05 22:55:48 -0700

    create array of ints and fill it

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of ints and fills it with values
+ * @min: minimum value in array
+ * @max: maximum value in array
+ * Return: pointer to newly created array or NULL if it fails
+ */
+int *array_range(int min, int max)
+{
+	int i, size;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
+	for (i = min; i <= max; i++)
+	{
+		arr[i] = i;
+	}
+	return (arr);
+}
