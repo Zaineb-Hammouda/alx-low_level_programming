9876cf0696f6eb017d4fb28be7a7c421c94b91f5
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-04-07 20:28:11 -0700

    convert binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-/**
- * rev_string - reverses a string in place
- *
- * @s: string to reverse
- * Return: A pointer to a character
- */
-char *rev_string(char *s)
-{
-	int len;
-	int head;
-	char tmp;
-	char *dest;
 
-	for (len = 0; s[len] != '\0'; len++);
-
-	dest = malloc(sizeof(char) * len + 1);
-	if (dest == NULL)
-		return (NULL);
-	memcpy(dest, s, len);
-	for (head = 0; head < len; head++, len--)
-	{
-		tmp = dest[len - 1];
-		dest[len - 1] = dest[head];
-		dest[head] = tmp;
-	}
-	return (dest);
-}
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b: pointer to string of binary
@@ -38,40 +10,20 @@ char *rev_string(char *s)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0, pow = 1;
-	size_t i;
-	char *buffer;
-	char *rev_b;
+	int i;
 
 	if (b == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * (strlen(b) + 1));
-	if (buffer == NULL)
-		return (0);
-	strcpy(buffer, b);
-	rev_b = rev_string(buffer);
-	if (rev_b == NULL)
-		return (0);
-	for (i = 0; i <= strlen(rev_b); i++)
+
+	for (i = strlen(b) - 1; i >= 0; i--)
 	{
-		/*if (rev_b[i] != 1 && rev_b[i] != 0)
-		{
-			printf("the fault is here\n");
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
-		}*/
-		if (rev_b[i] == 1)
+		if (b[i] == '1')
 		{
-			while (i != 0)
-			{
-				pow *= 2;
-				--i;
-			}
 			result += pow;
-			printf("%u\n", result);
 		}
-		else if (rev_b[i] == 0)
-			continue;
+		pow *= 2;
 	}
-	free(buffer);
-	free(rev_b);
 	return (result);
 }
