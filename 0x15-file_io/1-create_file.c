474d14a42cc263aa58c0f224af2f58f28429b13b
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-04-13 17:57:25 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_EXCL | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_EXCL | O_CREAT, 0600);
 	if (fd == -1)
 	{
 		if (errno == EEXIST)
-			fd = open(filename, O_CREAT | O_WRONLY);
+			fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 		else
 			return (-1);
 	}
