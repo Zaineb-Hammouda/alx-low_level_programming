ee325c1a58d907e8adc5897a2baed82e6f837fb4
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-04-13 18:42:22 -0700

    fixed task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-
+#include <errno.h>
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file
@@ -16,18 +16,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND | O_EXCL);
 	if (fd == -1)
+	{
+		if (errno == EEXIST)
+			return (1);
 		return (-1);
-
+	}
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
 		num_w = write(fd, text_content, len);
 	}
 
-	close(fd);
-	if (len != num_w)
+	if (len != num_w || close(fd) == -1)
 		return (-1);
 	return (1);
 }
