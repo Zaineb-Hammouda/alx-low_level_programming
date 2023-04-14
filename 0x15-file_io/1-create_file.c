0c1261c09e10e636a1dd404e0555e39e188b6f19
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-04-13 17:47:08 -0700

    fixed library error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-#include <stdio.h>
+#include <errno.h>
 
 /**
  * create_file - creates a file
@@ -18,10 +18,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_WRONLY | O_EXCL | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return (-1);
-
+	{
+		if (errno == EEXIST)
+			fd = open(filename, O_CREAT | O_WRONLY);
+		else
+			return (-1);
+	}
 	if (text_content != NULL)
 	{
 		len = strlen(text_content);
