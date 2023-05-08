2b94125f827308bfaab1cd8dc928bea0210e980a
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-05-09 01:30:03 +0200

    task 2 - append text to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: name of the file to append to
+ * @text_content: text to append
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, num_w, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND | O_EXCL);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		num_w = write(fd, text_content, len);
+		if (num_w == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	else
+		return (-1);
+	return (1);
+}
