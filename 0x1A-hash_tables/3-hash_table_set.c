032a80606e6fc47215994d1bd944466042c497bf
Zaineb-Hammouda <zaineb.hammouda.zee@gmail.com>
2023-07-08 02:39:54 +0200

    fixed segmentation fault

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,25 +9,40 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int hash;
+	unsigned long int idx;
 	hash_node_t *node;
 
 	if (key == NULL || ht == NULL)
 		return (0);
-	hash = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[hash];
+	idx = key_index((unsigned char *)key, ht->size);
+	node = ht->array[idx];
 	if (node == NULL)
 	{
-		node->key = (char *)key;
-		node->value = (char *)value;
+		node = malloc(sizeof(hash_node_t));
+		if (!node)
+			return (0);
+		node->key = malloc(strlen(key) + 1);
+		if (node->key == NULL)
+			return (0);
+		node->value = malloc(strlen(value) + 1);
+		if (node->value == NULL)
+			return (0);
+		strcpy(node->key, key);
+		strcpy(node->value, value);
+		node->next = NULL;
+		ht->array[idx] = node;
 		return (1);
 	}
-	while (node != NULL)
+	while (node)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
 			if (strcmp(node->value, value) == 0)
 				return (1);
+			free(node->value);
+			node->value = malloc(strlen(value) + 1);
+			if (node->value == NULL)
+				return (0);
 			strcpy(node->value, value);
 			return (1);
 		}
@@ -46,8 +61,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 		strcpy(node->key, key);
 		strcpy(node->value, value);
-		node->next = ht->array[hash];
-		ht->array[hash] = node;
+		node->next = ht->array[idx];
+		ht->array[idx] = node;
 		return (1);
 	}
 	return (0);
